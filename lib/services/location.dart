4694a29d446d0e2251dadc6bde3fd5147ecf0dbe
XD-OB <ob-96@hotmail.com>
2020-02-29 12:03:37 +0100

    update geolocation params

--- lib/services/location.dart
+++ lib/services/location.dart
@@ -8,7 +8,7 @@ class Location {
   Future<void>   getCurrentLocation() async {
     try {
       Position position = await Geolocator().getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.lowest,
+        desiredAccuracy: LocationAccuracy.medium,
       );
       latitude = position.latitude;
       longitude = position.longitude;
